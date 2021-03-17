b10d113f32cf944501423ffd2fab5e90399ba036
butterbaby4427 <aet395@nyu.edu>
2021-03-16 22:06:18 -0400

    new

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'MNMPUY8eK8uKVGucgkgP9RkuD',
+  consumer_secret:      'bGz6HHHCck8a5b45ywh8TUbH0fzkYj5XZ4xUOUostyiu2gMyEg',
+  access_token:         '1366485082442186756-28yWVsuCdH1UVtEv5drKSotTqqq4TW',
+  access_token_secret:  'Ae8a9K2faCJ0Z83tKaBpq4XkSblRGSqUGpMmp6kgtw5qB'
+};
