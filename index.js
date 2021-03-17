b6df5932775d6b6b58a5b710e66873f804c55604
butterbaby4427 <aet395@nyu.edu>
2021-03-16 21:52:29 -0400

    03-16-21

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ const newsKey = "Bf6a1033fd4049c2bb341767126f5ee3";
 
 var T = new Twit(config);
 var title;
-var titleArray;
-var newArray = [];
+var titleArray = [];
 var Nurl;
 var Turl;
 var Mdata;
 var Tdata;
-var currWord;
-var currSyn;
 var tweet = "";
 
 const request = require('request');
@@ -21,8 +18,8 @@ botTweet();
 setInterval(botTweet,1000*60*5); //TWEETS EVERY 5 MIN!!!
 
 function botTweet(error,response,body){
+
 	Nurl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + newsKey;
-  	
   	request(Nurl, getMData);
 
 	function getMData(error,response,body){
@@ -31,10 +28,9 @@ function botTweet(error,response,body){
 		}
 		MData = JSON.parse(body);
 		title = MData.articles[Math.floor(Math.random()*MData.articles.length)].title;
-		//title = MData.articles[1].title;
 		console.log(title);
-		console.log(shmix(title));
-		tweet = newArray.join(" ");
+		shmix(title);
+		// 
 	}
 
 	function tweeted(error,data,response){
@@ -44,42 +40,54 @@ function botTweet(error,response,body){
 	}
 }
 
-function shmix(words){
-	var index;
-
+function shmix(words){//LOOPS THROUGH WORDS IN TITLE AND SWITCHES THEM OUT 
+	var currWord;
+	var currSyn; 
+	
 	function getTData(error,response,body){
 		if(error){
 			console.log(error);
 		}
 		TData = JSON.parse(body);
-		//console.log(TData);
-		currSyn = TData[0];
+		//NEWSAPI IS WEIRD. THIS NEEDS TO BE HERE AS A RESULT. :)
+		if(TData[0] === undefined){
+		 	currSyn = currWord;
+		} else 
+		if(typeof TData[0] === 'string'){
+			currSyn = TData[0];
+		} else if ("meta" in TData[0]){
+			currSyn = TData[0].meta.syns[0][Math.floor(Math.random()*TData[0].meta.syns[0].length)];
+		} 
+		if (typeof currSyn != 'string'){
+			currSyn = currWord;
+		}
+		console.log(currWord);
 		console.log(currSyn);
-		//console.log(index);
-		newArray[index] = currSyn;
-		tweet = newArray.join(" ");
-		console.log("tweet");
+		titleArray.splice(number,1,currSyn);
+		tweet = titleArray.join(" ");
+		console.log(tweet);
 		T.post('statuses/update', {status: tweet}, tweeted);
-
-		function tweeted(error, data, response){
-	        if(error){
-	        console.log(error);
-	        } else {
-	        console.log("You're doing great! " + data.text);
-	        }
-	    } 
+		function tweeted(error,data,response){
+			if(error){
+				console.log(error);
+			}
+		}
 	}
 
-	var i;
+	var longest;
+	var lgth = 0;
+	var number;
 	titleArray = words.split(" ");
-	newArray = titleArray;
-	//for(i =0; i < 1; i++){
-		//console.log(titleArray.length);
-		index = Math.floor(Math.random()*titleArray.length);
-		currWord = titleArray[index];
-		Turl = "https://dictionaryapi.com/api/v3/references/thesaurus/json/"+currWord+"?key="+merriamKey;
-		request(Turl, getTData);
-	//}
-	//console.log(newArray);
-	return newArray.join(' ');
+
+	for (var i = 0; i < titleArray.length; i++) {
+	  if (titleArray[i].length > lgth) {
+	    lgth = titleArray[i].length;
+	    longest = titleArray[i];
+	    number = i;
+	  }
+	}
+
+	currWord = longest;
+	Turl = "https://dictionaryapi.com/api/v3/references/thesaurus/json/"+currWord+"?key="+merriamKey;
+	request(Turl, getTData);
 }
